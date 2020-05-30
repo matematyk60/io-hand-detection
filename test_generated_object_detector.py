bb07ef2ebbfd6f6c859e35eaa60c43b13a2d2be0
matematyk60 <dawid.gdk@gmail.com>
2020-05-30 02:40:58 +0200

    init commit

---
+++ test_generated_object_detector.py
@@ -0,0 +1,21 @@
+import sys
+import dlib
+import cv2
+import imutils
+
+
+detector = dlib.simple_object_detector("detector.svm")
+cam = cv2.VideoCapture(0)
+color_green = (0,255,0)
+line_width = 3
+while True:
+    ret_val, img_ = cam.read()
+    img = imutils.resize(img_, 1200)
+    rgb_image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    dets = detector(rgb_image)
+    for det in dets:
+        cv2.rectangle(img,(det.left(), det.top()), (det.right(), det.bottom()), color_green, line_width)
+    cv2.imshow('my webcam', img)
+    if cv2.waitKey(1) == 27:
+        break  # esc to quit
+cv2.destroyAllWindows()
